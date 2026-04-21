59768b8bdfcc8d66763df8fc493c0ac69d14f128
Redon4 <211563626+redon4@users.noreply.github.com>
2026-04-21 14:17:18 +0200

    v0.0.5: changed file structure and other stuff

--- gen_mines.py
+++ gen_mines.py
@@ -1,7 +1,7 @@
 import random # maybe, this is test stuff
 
 def gen_mines(w, h):
-    mines = [[random.randint(0, 1) for _ in range(w)] for _ in range(h)]
+    mines = [[random.choice([0, 0, 0, 0, 0, 0, 1]) for _ in range(w)] for _ in range(h)]
     return mines
 
 
