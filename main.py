d3028d749509f1fdc9e104014b4074803ff4e915
Redon4 <211563626+Redon4@users.noreply.github.com>
2026-04-21 11:59:28 +0200

    v0.0.1: Initial Setup

---
+++ main.py
@@ -0,0 +1,17 @@
+#imports:
+
+
+
+#main class
+
+class MineWalker:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def start(cls):
+        # will start the game
+        pass
+
+    
+
