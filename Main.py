fb4a890d2a2b5c5459ad5eead47a79bb3b50b035
Redon4 <daniel.ferger@icloud.com>
2026-04-21 14:17:18 +0200

    v0.0.5: changed file structure and other stuff

---
+++ Main.py
@@ -0,0 +1,7 @@
+import curses
+from MineWalker import MineWalker
+
+if __name__ == "__main__":
+    width, height = 10, 10
+
+    curses.wrapper(MineWalker.run, width, height)
--- MineWalker.py
+++ MineWalker.py
@@ -12,48 +12,57 @@ class MineWalker:
         self.x, self.y = 0, 0
         self.board = gen_mines(width, height)
         self.game = True
-        self.discovered = set()
+        self.discovered = {(0, 0)}
         self.died = False
 
     @classmethod
-    def start(cls, stdscr, width=10, height=10):
+    def run(cls, stdscr, width=10, height=10):
         clas = cls(stdscr, width, height)
 
         while clas.game:
+            clas.board[clas.y][clas.x] = 3
             clas.draw()
             clas.get_input()
         if clas.died:
             clas.draw()
-            clas.stdscr.addstr("Game Over! Press any key to exit.")
-            clas.stdscr.getch()
+            clas.stdscr.addstr("Game Over! Press enter or spaceto exit.")
+            while True:
+                key = clas.stdscr.getkey()
+                if key == "\n" or key == " ":
+                    break
 
     def draw(self):
         self.stdscr.clear()
 
-        
+
         self.draw_board=[]
         for y in range(len(self.board)):
             self.draw_board.append([])
             for x in range(len(self.board[0])):
                 if self.board[y][x] == 3:
                     self.draw_board[y].append("X")
+
                 elif (x, y) in self.discovered:
                     self.draw_board[y].append(" ")
+
+                elif self.died and self.board[y][x] == 1: # wenn du durch eine Mine gestorben bist, zeige alle Minen an
+                    self.draw_board[y].append("*")
+
                 else:
                     self.draw_board[y].append("O")
-            
-             
-        
+
+
+
         for row in self.draw_board:
             self.stdscr.addstr(" ".join(row) + "\n")
-        
+
         self.stdscr.refresh()
 
 
     def get_input(self):
         inp = self.stdscr.get_wch()
         self.board[self.y][self.x] = 0
-        
+
         match inp:
             case "w" | curses.KEY_UP:
                 self.y -= 1
@@ -70,20 +79,10 @@ class MineWalker:
         self.x = max(0, min(self.x, len(self.board[0])-1))
         self.y = max(0, min(self.y, len(self.board)-1))
 
-        if self.board[self.y][self.x] == 1:
+        if self.board[self.y][self.x] == 1: # wenn du in einer Mine bist
             self.died = True
             self.game = False
             return
-        
-        self.board[self.y][self.x] = 3
-        self.discovered.add((self.x, self.y))
 
-        
-
-def main(stdscr):
-    MineWalker.start(stdscr, width=10, height=10)
-
-
-
-
-curses.wrapper(main) 
+        self.board[self.y][self.x] = 3 # akutuelle position = spieler
+        self.discovered.add((self.x, self.y)) # du warst hier, es ist ein set
