0d81cb5bde35b13fc1501e248b4917f9eff22964
Redon4 <211563626+Redon4@users.noreply.github.com>
2026-04-21 13:58:09 +0200

    v0.0.4: implemented mines, you can die now

--- gen_mines.py
+++ gen_mines.py
@@ -3,3 +3,10 @@ import random # maybe, this is test stuff
 def gen_mines(w, h):
     mines = [[random.randint(0, 1) for _ in range(w)] for _ in range(h)]
     return mines
+
+
+if __name__ == "__main__":
+    w, h = 10, 10
+    mines = gen_mines(w, h)
+    for row in mines:
+        print(" ".join(str(x) for x in row))
