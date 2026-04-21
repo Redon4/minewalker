26e45980403562ec4ce4138c26069b66cdba30cc
Redon4 <211563626+redon4@users.noreply.github.com>
2026-04-21 12:44:42 +0200

    v0.0.3: Making gen_mines.py a module

--- gen_mines.py
+++ gen_mines.py
@@ -2,7 +2,4 @@ import random # maybe, this is test stuff
 
 def gen_mines(w, h):
     mines = [[random.randint(0, 1) for _ in range(w)] for _ in range(h)]
-    for i in mines:
-        print(i)
-
-gen_mines(5, 5)
+    return mines
