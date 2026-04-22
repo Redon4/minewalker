239bbeafbd8df6d4fe90374f7002296e1aec0056
Redon4 <daniel.ferger@icloud.com>
2026-04-22 18:58:23 +0200

    v0.1.1: stole color system from myself (redon4x2)

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import curses
 from MineWalker import MineWalker
 
 if __name__ == "__main__":
-    width, height = 10, 10
+    width, height = 20, 20
 
     curses.wrapper(MineWalker.run, width, height)
     
