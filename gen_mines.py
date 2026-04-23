6048be5dcec6bbb963bf5fd64c9f095a4cf69a00
Redon4 <daniel.ferger@icloud.com>
2026-04-23 12:46:35 +0200

    v0.1.5: added highscore system, added better colors

--- gen_mines.py
+++ gen_mines.py
@@ -1,6 +1,6 @@
 import random # maybe, this is test stuff
 
-def gen_mines(w, h, density=0.2):
+def gen_mines(w, h, density):
     choice_list = [0] * int(1 / density - 1) + [1] # proper desity
     mines = [[random.choice(choice_list) for _ in range(w)] for _ in range(h)] # damit die change for 1 kleiner ist
     mines[0][:2] = [0, 0] # spawn area
