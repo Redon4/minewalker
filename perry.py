2c081759e4f66e2352c0123be304237187bda85c
Redon4 <211563626+Redon4@users.noreply.github.com>
2026-04-23 09:47:28 +0200

    v0.1.3: made perry colors reversable and cursor glowing

--- perry.py
+++ perry.py
@@ -8,18 +8,23 @@ class Perry:
         self.colors = {}
         curses.use_default_colors()
 
-    def color(self, NAME, BACK=0):
+    def color(self, NAME, BACK=0, reverse=False, rev=False):
         pair_id = f"{str(NAME).upper()}_{str(BACK).upper()}"
 
         if not pair_id in self.colors and isinstance(NAME, str):
             try:
-                self.colors[pair_id] = [len(self.colors) + 1, getattr(curses, f"COLOR_{NAME.upper()}"), -1 if not BACK else getattr(curses, f"COLOR_{str(BACK).upper()}")]
+                self.colors[pair_id] = [len(self.colors) + 1]
+                self.colors[pair_id].append(getattr(curses, f"COLOR_{NAME.upper()}"))
+                self.colors[pair_id].append(-1 if not BACK else getattr(curses, f"COLOR_{str(BACK).upper()}"))
             except AttributeError:
                 raise ValueError(f"!!!Not a curses color!!!:\t{NAME} or {BACK}")
+            
             color = self.colors[pair_id]
             curses.init_pair(color[0], color[1], color[2])
 
-        elif isinstance(NAME, int):
+        elif isinstance(NAME, int) and not BACK:
             return curses.color_pair(NAME)
 
+        if reverse or rev:
+            return curses.color_pair(self.colors[pair_id][0]) | curses.A_REVERSE
         return curses.color_pair(self.colors[pair_id][0])
