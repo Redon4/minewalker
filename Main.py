1e8eb91e5c653a41d2dadc41d2e41445e919f26a
Redon4 <211563626+redon4@users.noreply.github.com>
2026-04-23 15:21:32 +0200

    v0.2.0: Now only 10% mines

--- Main.py
+++ Main.py
@@ -3,6 +3,6 @@ from MineWalker import MineWalker
 
 if __name__ == "__main__":
     width, height = 20, 20
-    density = 0.2
+    density = 0.1
 
     curses.wrapper(MineWalker.run, width, height, density)
