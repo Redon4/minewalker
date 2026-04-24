92c48f0b1dcb52ff72cf7257649c9f4c7f70597e
Redon4 <211563626+Redon4@users.noreply.github.com>
2026-04-24 20:57:27 +0200

    v0.3.1: added diagonal marking, updated README.md, renamed file

--- Singleplayer.py
+++ Singleplayer.py
@@ -4,6 +4,7 @@ import curses
 from gen_mines import gen_mines, clear_path
 from perry import Perry
 from score import save_score, load_score
+import utils
 
 
 # main class
@@ -28,7 +29,10 @@ class MineWalker:
         curses.curs_set(0)
 
     @classmethod # damit man keine instance machen muss
-    def run(cls, stdscr, width=10, height=10, density=0.2):
+    def run(cls, stdscr, width=10, height=10, density=0.2, **kwargs):
+        if kwargs.get("timed", False):
+            stdscr.nodelay(True)
+        cls.easy_mode = kwargs.get("easy_mode", False)
         while True:
             clas = cls(stdscr, width, height, density)
 
@@ -39,13 +43,16 @@ class MineWalker:
             msg = "You won! " if clas.won else ("You quit! " if not clas.died else "Game Over! ")
             clas.draw()
             clas.stdscr.move(len(clas.board)+2, 0)
-            clas.stdscr.addstr(f"{msg}Press enter to exit or space to play again.")
+            clas.stdscr.addstr(f"{msg}Press \"q\" to exit, space or enter to play again.")
             while True:
-                key = clas.stdscr.getkey()
-                if key == "\n": # enter
-                    return # beende
-                elif key == " ": # space
-                    break # starte neu
+                try:
+                    key = clas.stdscr.getkey()
+                except:
+                    continue
+                if key in ("\n", " "): # enter
+                    break # restart
+                elif key == "q": # space
+                    return # quit
             save_score(max(clas.highscore, len(clas.discovered) - 1)) # save the highscore
 
 
@@ -53,7 +60,7 @@ class MineWalker:
         self.stdscr.erase()
 
         self.stdscr.addstr(f"Score: {len(self.discovered) - 1}")
-        self.stdscr.addstr("\t| WASD or arrow keys to move, space or enter to quit. \"m\" or \"e\" + direction to mark/unmark a cell." + "\n")
+        self.stdscr.addstr("\t| WASD or arrow keys to move; space, enter or \"q\" to exit. \"m\" or \"e\" + direction to mark/unmark a cell." + "\n")
 
 
         around_area = self.board[max(0, self.y-1):min(self.y+2, len(self.board))]
@@ -105,20 +112,31 @@ class MineWalker:
 
 
     def get_input(self):
-        inp = self.stdscr.get_wch()
+
+        try:
+            inp = self.stdscr.get_wch()
+        except:
+            inp = None
         self.board[self.y][self.x] = 0
 
+        move=[0, 0] # y, x
         match inp:
             case "w" | curses.KEY_UP:
-                self.y -= 1
+                move[0] = -1
             case "s" | curses.KEY_DOWN:
-                self.y += 1
+                move[0] = 1
             case "a" | curses.KEY_LEFT:
-                self.x -= 1
+                move[1] = -1
             case "d" | curses.KEY_RIGHT:
-                self.x += 1
-            case "m" | "e": # mark
-                inp2 = self.stdscr.get_wch()
+                move[1] = 1
+            case "m" | "e" | "f": # mark
+                while True:
+                    try:
+                        inp2 = self.stdscr.get_wch()
+                    except:
+                        pass
+                    else:
+                        break
                 offset=[0, 0]
                 match inp2:
                     case "w" | curses.KEY_UP:
@@ -129,24 +147,35 @@ class MineWalker:
                         offset[1] = -1
                     case "d" | curses.KEY_RIGHT:
                         offset[1] = 1
+                    #diagonal
+                    case "e":
+                        offset = [-1, 1]
+                    case "q":
+                        offset = [-1, -1]
+                    case "c":
+                        offset = [1, 1]
+                    case "y" | "z":
+                        offset = [1, -1]
 
-                x = max(0, min(len(self.board[0]) -1, self.x + offset[1]))
-                y = max(0, min(len(self.board) -1, self.y + offset[0]))
-                if (x, y) in self.marked:
-                    self.marked.remove((x, y))
-                else:
-                    self.marked.add((x, y))
-            case " " | "\n": # quit
+                x, y = utils.clamp_to_matrix(self.x + offset[1], self.y + offset[0], self.board)
+                if (x, y) not in self.discovered:
+                    if (x, y) in self.marked:
+                        self.marked.remove((x, y))
+                    else:
+                        self.marked.add((x, y))
+            case " " | "\n" | "q" : # quit
                 self.game = False
 
 
-        self.x = max(0, min(self.x, len(self.board[0])-1))
-        self.y = max(0, min(self.y, len(self.board)-1))
+        # self.x = max(0, min(self.x, len(self.board[0])-1))
+        # self.y = max(0, min(self.y, len(self.board)-1))
+        if (self.x + move[1], self.y + move[0]) in self.marked and self.easy_mode: # dont move into makred cells on easy mode
+            return
+        self.x, self.y = utils.clamp_to_matrix(self.x + move[1], self.y + move[0], self.board)
 
-        if self.board[self.y][self.x] == 1: # wenn du in einer Mine bist
+        if self.board[self.y][self.x] == 1: # if youre in a mine
             self.died = True
             self.game = False
             return
 
-        self.board[self.y][self.x] = 3 # akutuelle position = spieler
-        self.discovered.add((self.x, self.y)) # du warst hier, es ist ein set
+        self.discovered.add((self.x, self.y))
--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
 def clamp(start, value, end):
     return max(start, min(value, end))
+
+def clamp_to_matrix(x, y, matrix): # matrix width has to be the same for every row
+    r=[0, 0]
+    r[0] = clamp(0, x, len(matrix[0]) - 1)
+    r[1] = clamp(0, y, len(matrix) - 1)
+
+    return [r[0], r[1]]
