ea98fbb9e9018477428e4f1a4fb15798944ec1dc
Redon4 <211563626+Redon4@users.noreply.github.com>
2026-04-27 20:26:22 +0200

    v0.3.6: Testing Stuff

--- Main.py
+++ Main.py
@@ -4,6 +4,6 @@ import game_modes as game
 
 if __name__ == "__main__":
     width, height = 20, 20
-    density = 0.25
+    density = 0.26
 
     curses.wrapper(game.singleplayer, width, height, density, timed=True, easy_mode=True)
